da4e801197274cbca0f718ba88b2208b65f93e18
Benjamin Fontana <ben192003@gmail.com>
2025-03-14 11:13:25 +0100

    feat: phone formatting and validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,6 +4,10 @@ class Person < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :status, presence: true
+  validates :phone_number, presence: true, format: {
+    with: /\A(?:(?:\+\d{1,3}\s?)?\d{2,3}(?:\s?\d{3}){1,2}(?:\s?\d{2}){1,2})\z/,
+    message: "must be a valid phone number format like '078 677 20 06' or '+41 78 682 25 65'"
+  }
 
   accepts_nested_attributes_for :address
 
