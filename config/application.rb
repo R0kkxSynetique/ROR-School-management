fb77afca648eeb7eb8bb0e75e8e9a1f4f209df3b
Benjamin Fontana <ben192003@gmail.com>
2025-03-20 15:05:38 +0100

    chore: set server timezone to EU/ZH

--- config/application.rb
+++ config/application.rb
@@ -39,8 +39,8 @@ module SchoolManagement
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+
+    config.time_zone = "Europe/Zurich"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
